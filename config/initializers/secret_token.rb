9ad99a766205bf0f1d425599386c3881baea8dfb
Robson Carvalho <robsonpca@gmail.com>
2014-08-08 17:25:45 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HQBRStore6::Application.config.secret_key_base = 'a4782c7cf4595c1fd5f82ea1acfe56be232b4d7819d0267252f5a6dbb898656a33ce2f3278c1a713dc7092707c92ed75411e515216095dca654f7d5a5f82b7ae'
